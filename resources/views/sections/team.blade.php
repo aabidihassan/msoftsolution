b8790ba3904b86386bffdbce1e046612095d9666
aabidihassan <aabidihassan2@gmail.com>
2022-01-11 11:47:26 +0100

    add zack to the team

--- resources/views/sections/team.blade.php
+++ resources/views/sections/team.blade.php
@@ -12,7 +12,7 @@
                     <div class="team-block">
                         <div class="inner-box">
                             <div class="image-box">
-                                <a href="about.html"><img src="images/resource/team-1.jpg" alt=""></a>
+                                <a href="about.html"><img src="/images/Team/zack daoudi.png" alt="Zakaria Daoudi"></a>
                                 <ul class="social-links clearfix">
                                     <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
                                     <li><a href="#"><span class="fab fa-twitter"></span></a></li>
@@ -21,12 +21,10 @@
                                 </ul>
                             </div>
                             <div class="lower-box">
-                                <h5><a href="#">Mon cheff Hassane</a></h5>
-                                <div class="designation">CEO 1</div>
+                                <h5><a href="#">Zakaria Daoudi</a></h5>
+                                <div class="designation">Développeur Full stack JAVA/Angular</div>
                             </div>
                         </div>
-                    </div>
-                    <div class="team-block">
                         <div class="inner-box">
                             <div class="image-box">
                                 <a href="about.html"><img src="images/resource/team-1.jpg" alt=""></a>
@@ -38,42 +36,8 @@
                                 </ul>
                             </div>
                             <div class="lower-box">
-                                <h5><a href="#">Mon cheff Zakaria</a></h5>
-                                <div class="designation">CEO 2</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-block">
-                        <div class="inner-box">
-                            <div class="image-box">
-                                <a href="about.html"><img src="images/resource/team-1.jpg" alt=""></a>
-                                <ul class="social-links clearfix">
-                                    <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
-                                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                                    <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                                    <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                                </ul>
-                            </div>
-                            <div class="lower-box">
-                                <h5><a href="#">Asma Daoudi</a></h5>
-                                <div class="designation">CEO 2</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-block">
-                        <div class="inner-box">
-                            <div class="image-box">
-                                <a href="about.html"><img src="images/resource/team-1.jpg" alt=""></a>
-                                <ul class="social-links clearfix">
-                                    <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
-                                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                                    <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                                    <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                                </ul>
-                            </div>
-                            <div class="lower-box">
-                                <h5><a href="#">Otman</a></h5>
-                                <div class="designation">CEO 2</div>
+                                <h5><a href="#">Hassan Aabidi</a></h5>
+                                <div class="designation">Ingénieur logiciel</div>
                             </div>
                         </div>
                     </div>
