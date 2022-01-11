12bb6955d81666572eeae25ba21df5e73386c0c3
aabidihassan <aabidihassan2@gmail.com>
2022-01-11 12:02:46 +0100

    fix all team

--- resources/views/sections/team.blade.php
+++ resources/views/sections/team.blade.php
@@ -12,7 +12,7 @@
                     <div class="team-block">
                         <div class="inner-box">
                             <div class="image-box">
-                                <a href="about.html"><img src="/images/Team/zack daoudi.png" alt="Zakaria Daoudi"></a>
+                                <a href="about.html"><img src="/images/Team/zack daoudi.png" alt=""></a>
                                 <ul class="social-links clearfix">
                                     <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
                                     <li><a href="#"><span class="fab fa-twitter"></span></a></li>
@@ -25,9 +25,11 @@
                                 <div class="designation">Développeur Full stack JAVA/Angular</div>
                             </div>
                         </div>
+                    </div>
+                    <div class="team-block">
                         <div class="inner-box">
                             <div class="image-box">
-                                <a href="about.html"><img src="images/resource/team-1.jpg" alt=""></a>
+                                <a href="about.html"><img src="/images/Team/zack daoudi.png" alt=""></a>
                                 <ul class="social-links clearfix">
                                     <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
                                     <li><a href="#"><span class="fab fa-twitter"></span></a></li>
@@ -37,7 +39,41 @@
                             </div>
                             <div class="lower-box">
                                 <h5><a href="#">Hassan Aabidi</a></h5>
-                                <div class="designation">Ingénieur logiciel</div>
+                                <div class="designation">Ingenieur logiciel</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="team-block">
+                        <div class="inner-box">
+                            <div class="image-box">
+                                <a href="about.html"><img src="/images/Team/zack daoudi.png" alt=""></a>
+                                <ul class="social-links clearfix">
+                                    <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+                                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                                    <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+                                    <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                                </ul>
+                            </div>
+                            <div class="lower-box">
+                                <h5><a href="#">Yasser Ezzahri</a></h5>
+                                <div class="designation">Développeur Back-End .NET</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="team-block">
+                        <div class="inner-box">
+                            <div class="image-box">
+                                <a href="about.html"><img src="/images/Team/zack daoudi.png" alt=""></a>
+                                <ul class="social-links clearfix">
+                                    <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+                                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
+                                    <li><a href="#"><span class="fab fa-instagram"></span></a></li>
+                                    <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+                                </ul>
+                            </div>
+                            <div class="lower-box">
+                                <h5><a href="#">Zouhir Yacouri</a></h5>
+                                <div class="designation">React & Ui/Ux designer</div>
                             </div>
                         </div>
                     </div>
